90551dfc05de82e80e7e7e87362139edb76eec71
Garrett Faber <fabergarrett@gmail.com>
2019-06-17 12:22:31 -0500

    Moving funtest.cpp to c++ folder

--- funtest.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(){
-  int x;
-  cout << "What number do you want to output?" << endl;
-  cin >> x;
-  cout << "The number that you entered is: " << x << endl;
-}
